cfebfab28ce218b376830d087caa5ff61a755545
Fiveolioo <96richardohc@gmail.com>
2021-01-31 21:33:51 -1000

    commit tree 6c5049f4050773b384609140954bdd7b29d420e6

--- src/components/SearchableMovieReviewsContainer.js
+++ src/components/SearchableMovieReviewsContainer.js
@@ -4,6 +4,33 @@ import MovieReviews from './MovieReviews'
 
 const NYT_API_KEY = 'dGpQ5OmGP2SgfvZimlpCUoF4iOag9qzZ';
 const URL = 'https://api.nytimes.com/svc/movies/v2/reviews/search.json?'
-            + `api-key=${NYT_API_KEY}`;
+            + `api-key=${NYT_API_KEY}&query=`;
 
 // Code SearchableMovieReviewsContainer Here
+class SearchableMovieReviewsContainer extends Component {
+    constructor() {
+    super();
+        this.state = {
+            reviews: [],
+            searchTerm: ''
+        }
+    }
+
+    submitEvent(event) {
+        event.preventDefault();
+        fetch(URL.concat(this.state.searchTerm))
+            .then(resp => resp.json())
+            .then(resp => this.setState({ reviews: resp.results }));
+    }
+    
+    render() {
+    return(
+        <div className="searchable-movie-reviews">
+        <h3>The Searched Movie Reviews:</h3>
+        <MovieReviews reviews={this.state.reviews} />
+        </div>
+        );
+    }
+}
+
+export default SearchableMovieReviewsContainer;
